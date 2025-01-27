720986517d4ec3133defc8295fccbea052834cd9
Paulo Borges <victorborges296@hotmail.com>
2025-01-27 20:12:21 -0300

    UserInformation component created + PostForm (needs some features next)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
+import PostForm from "@/components/PostForm";
+import UserInformation from "@/components/UserInformation";
+
 export default function Home() {
   return (
-    <main className="flex">
-      <h1>LinkedIn Clone</h1>
-    </main>
+    <div className="grid grid-cols-8 mt-5 sm:px-5">
+      <section className="hidden md:inline md:col-span-2">
+        <UserInformation />
+      </section>
+
+      <section className="col-span-full md:col-span-6 xl:col-span-4 xl:max-w-xl mx-auto w-full">
+        {/* PostForm */}
+        <PostForm />
+        {/* PostFeed */}
+      </section>
+
+      <section className="hidden xl:inline justify-center col-span-2">
+        {/* Widget */}
+      </section>
+    </div>
   );
 }
